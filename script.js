3104f8f5a81a10acae6a00c4f77cbc2189d45e51
5231 <31521303+5231@users.noreply.github.com>
2017-11-15 14:06:42 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+// JavaScript is capitalized using "camel case": https://en.wikipedia.org/wiki/Camel_case
+// Check out the power of the classList property: https://www.w3schools.com/jsref/prop_element_classlist.asp
+
+function makeOpaque1(e){
+	e.classList.toggle("opaque1");
+}
+function makeOpaque2(e){
+	e.classList.toggle("opaque2");
+}
+function makeOpaque3(e){
+	e.classList.toggle("opaque3");
+}
+function makeOpaque4(e){
+	e.classList.toggle("opaque4");
+}
+function makeOpaque5(e){
+	e.classList.toggle("opaque5");
+}
