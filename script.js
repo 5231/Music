9ea812d61c9e04b16dee872217fb23def70858b7
5231 <31521303+5231@users.noreply.github.com>
2017-11-29 13:17:09 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// JavaScript is capitalized using "camel case": https://en.wikipedia.org/wiki/Camel_case
-// Check out the power of the classList property: https://www.w3schools.com/jsref/prop_element_classlist.asp
-
 function makeOpaque1(e){
 	e.classList.toggle("opaque1");
 }
